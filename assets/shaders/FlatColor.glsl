f3630516731ad9418311a076959b4f679c3832f7
longerlong <longzeren@g.harvard.edu>
2021-08-18 09:14:09 +0800

    [Cube] Render a rotating cube.

--- assets/shaders/FlatColor.glsl
+++ assets/shaders/FlatColor.glsl
@@ -3,8 +3,12 @@
 
 layout (location = 0) in vec3 a_Pos;
 
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_ViewMatrix;
+uniform mat4 u_ProjectionMatrix;
+
 void main() {
-    gl_Position = vec4(a_Pos, 1.0f);
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * vec4(a_Pos, 1.0f);
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Shader.h"
 
 int main()
@@ -24,14 +27,28 @@ int main()
         return -1;
     }
 
+    glfwSwapInterval(1);
+
     // Main part
     float vertices[] = {
-            -0.5, -0.5, 0.0,
-            0.5, -0.5, 0.0,
-            0.0, 0.5, 0.0
+            -0.5, -0.5, -0.5,
+             0.5, -0.5, -0.5,
+             0.5, -0.5,  0.5,
+            -0.5, -0.5,  0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5, -0.5,
+             0.5,  0.5,  0.5,
+            -0.5,  0.5,  0.5,
     };
 
-    unsigned int indices[] = {0, 1, 2};
+    unsigned int indices[] = {
+            0, 1, 2, 2, 3, 0,
+            0, 4, 5, 5, 1, 0,
+            1, 5, 6, 6, 2, 1,
+            2, 6, 7, 7, 3, 2,
+            3, 7, 4, 4, 0, 3,
+            4, 5, 6, 6, 7, 4,
+    };
 
     uint32_t vao, vbo, ibo;
     glGenVertexArrays(1, &vao);
@@ -50,18 +67,31 @@ int main()
 
     Shader shader("../assets/shaders/FlatColor.glsl");
 
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+    glm::mat4 viewMatrix;
+    glm::mat4 projectionMatrix = glm::perspective(glm::radians(45.0f), (float)1440 / (float)900, 0.01f, 1000.0f);
+
+    shader.Bind();
+    shader.SetMat4("u_ModelMatrix", modelMatrix);
+    shader.SetMat4("u_ProjectionMatrix", projectionMatrix);
+
+    glm::vec3 position = glm::vec3(0.0, 0.0, 10);
+    glm::vec3 center = glm::vec3(0.0, 0.0, 0.0);
+    glm::vec3 up = glm::vec3(0.0, 1.0, 0.0);
+
     while (!glfwWindowShouldClose(window))
     {
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-
         glClearColor(0.1, 0.1, 0.1, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        position = glm::vec3(sin(glfwGetTime()), 0.0, cos(glfwGetTime())) * 10.f;
+
         shader.Bind();
+        viewMatrix = glm::lookAt(position, center, up);
+        shader.SetMat4("u_ViewMatrix", viewMatrix);
+
         glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
